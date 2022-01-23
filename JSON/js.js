eb414cfb7b8acc1474e115bf0abaacc5e277fcf2
yousra-hs <96131919+yousra-hs@users.noreply.github.com>
2022-01-23 21:06:02 +0100

    Add files via upload

---
+++ JSON/js.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $("#input").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#table tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+ 
